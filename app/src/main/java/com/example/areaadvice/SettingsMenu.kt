8eefbeff81cab73726c715f8487bb8d235776521
VishalV97 <vv198@scarletmail.rutgers.edu>
2019-11-30 17:38:11 -0500

    Settings Menu Changes

--- app/src/main/java/com/example/areaadvice/SettingsMenu.kt
+++ app/src/main/java/com/example/areaadvice/SettingsMenu.kt
@@ -1,24 +1,54 @@
 package com.example.areaadvice
 
-
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.Switch
+import android.widget.TextView
 
-/**
- * A simple [Fragment] subclass.
- */
 class SettingsMenu : Fragment() {
+
+    private lateinit var mContext: Context
+    private lateinit var recommendSensor: Switch
+    private lateinit var maxRadiusSeek: SeekBar
+    private lateinit var maxRadiusSeekText: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings_menu, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_settings_menu, container, false)
+
+        recommendSensor = view.findViewById(R.id.enableSensorRecToggle)
+        maxRadiusSeek = view.findViewById(R.id.radiusSeekBar)
+        maxRadiusSeekText = view.findViewById(R.id.radiusSeekBarText)
+
+        maxRadiusSeek.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                maxRadiusSeekText.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                maxRadiusSeekText.text = seekBar.progress.toString()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                maxRadiusSeekText.text = seekBar.progress.toString()
+            }
+
+        })
+
+
+        return view
     }
 
-
+    override fun onAttach(context: Context){
+        super.onAttach(context)
+        mContext = context
+    }
 }
