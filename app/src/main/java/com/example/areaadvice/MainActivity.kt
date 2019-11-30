329e8b7ec6703a9f9b5eda4065b3c55dbb9b83d9
Abhiek187 <achaudhuri2011@yahoo.com>
2019-11-30 18:31:57 -0500

    Clean MainActivity

--- app/src/main/java/com/example/areaadvice/MainActivity.kt
+++ app/src/main/java/com/example/areaadvice/MainActivity.kt
@@ -1,54 +1,21 @@
 package com.example.areaadvice
 
-import android.hardware.Sensor
-import android.hardware.SensorManager
 import android.os.Bundle
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navbar: BottomNavigationView
-    private lateinit var map: Button
-    private lateinit var clear:Button
-
-    private lateinit var sensorManager: SensorManager
-    private var currentTemp: Sensor? =null
-    private var light: Sensor?=null
-    private var prevTemp: Float? = null
-    private var prevLight:Float?=null
-    private var recommendations: String=""
-    private var recPrev: String=""
-    private var lightSen=true;
-
-   // private var manualRec=false;
-    /* Steps to hide your API key:
-     * 1. Create google_apis.xml in values folder (Git will ignore this file)
-     * 2. Add API key as string resource named google_places_key
-     * 3. Protect yourself from Chrysnosis by deleting his GitHub branch to avoid any running errors (sorry Krishna)
-     */
-    private lateinit var apiKey: String
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var locationRequest: LocationRequest
-    private lateinit var locationCallback: LocationCallback
-
-    var lat = 0.0
-    var lon = 0.0
-
-
+    private lateinit var navBar: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         changeFragment(Home())
-        navbar = findViewById(R.id.nav_bar)
-        navbar.setOnNavigationItemSelectedListener {item ->
+        navBar = findViewById(R.id.nav_bar)
+        navBar.setOnNavigationItemSelectedListener { item ->
             val fm = supportFragmentManager.beginTransaction()
             when(item.itemId){
                 R.id.Home ->{
