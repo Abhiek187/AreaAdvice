7bb5d9ce072abfadbf19826590244081196c779a
krishnamahadas <krishnamahadas@gmail.com>
2019-12-01 11:17:26 -0500

    shared preferences update
    updated the textviews for toggle, distance meter now holds,distance meter min is 1,

--- app/src/main/java/com/example/areaadvice/SettingsMenu.kt
+++ app/src/main/java/com/example/areaadvice/SettingsMenu.kt
@@ -23,6 +23,9 @@ class SettingsMenu : Fragment() {
     private lateinit var maxRadiusSeek: SeekBar
     private lateinit var maxRadiusSeekText: TextView
     private lateinit var radiusSeekBarText: TextView
+    private lateinit var sensorrecOnorOff: TextView
+    private lateinit var tempChoice: TextView
+    private lateinit var distChoice: TextView
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -45,6 +48,10 @@ class SettingsMenu : Fragment() {
         tempDegree=view.findViewById(R.id.tempChoiceToggle)
         disUnit=view.findViewById(R.id.distChoiceToggle)
 
+        sensorrecOnorOff=view.findViewById(R.id.sensorrecOnorOff)
+        tempChoice=view.findViewById(R.id.tempChoice)
+        distChoice=view.findViewById(R.id.distChoice)
+
         //intent.putExtra("radius", radiusSeekBarText.text )
         //intent.putExtra("senEnable",senEnable.isChecked)
 
@@ -56,22 +63,46 @@ class SettingsMenu : Fragment() {
         tempDegree.isChecked=sharedPref.getBoolean("tempUnit",true)
         disUnit.isChecked=sharedPref.getBoolean("disUnit",true)
         radiusSeekBarText.text= sharedPref.getString("radius","1").toString()
+        sensorrecOnorOff.text=sharedPref.getString("OnOff","On").toString()
+        tempChoice.text=sharedPref.getString("degree","°C").toString()
+        distChoice.text=sharedPref.getString("dist","km").toString()
 
 
         senEnable.setOnCheckedChangeListener { _, _ ->
             editor.putBoolean("senEnable", senEnable.isChecked)
+            if (senEnable.isChecked){
+                sensorrecOnorOff.text="On"
+            }
+            else{
+                sensorrecOnorOff.text="Off"
+            }
+            editor.putString("OnOff",sensorrecOnorOff.text.toString())
             editor.apply()
             editor.commit()
         }
 
         tempDegree.setOnCheckedChangeListener { _, _ ->
             editor.putBoolean("tempUnit", tempDegree.isChecked)
+            if (tempDegree.isChecked){
+                tempChoice.text="°C"
+            }
+            else{
+                tempChoice.text="°F"
+            }
+            editor.putString("degree",tempChoice.text.toString())
             editor.apply()
             editor.commit()
         }
 
         disUnit.setOnCheckedChangeListener { _, _ ->
             editor.putBoolean("disUnit",disUnit.isChecked)
+            if (disUnit.isChecked){
+                distChoice.text="km"
+            }
+            else{
+                distChoice.text="mi"
+            }
+            editor.putString("dist",distChoice.text.toString())
             editor.apply()
             editor.commit()
         }
@@ -79,6 +110,8 @@ class SettingsMenu : Fragment() {
         recommendSensor = view.findViewById(R.id.enableSensorRecToggle)
         maxRadiusSeek = view.findViewById(R.id.radiusSeekBar)
         maxRadiusSeekText = view.findViewById(R.id.radiusSeekBarText)
+
+        maxRadiusSeek.progress=sharedPref.getInt("bar",1)
 
         maxRadiusSeek.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
 
@@ -93,6 +126,7 @@ class SettingsMenu : Fragment() {
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 maxRadiusSeekText.text = seekBar.progress.toString()
                 editor.putString("radius",radiusSeekBarText.text.toString())
+                editor.putInt("bar",maxRadiusSeek.progress)
                 editor.apply()
                 editor.commit()
             }
